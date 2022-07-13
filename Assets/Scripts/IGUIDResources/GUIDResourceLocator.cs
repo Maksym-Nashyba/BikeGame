f2aa67d1a6f6de1ed160739ec727ad8dab767eef
Maksym-Nashyba <nashybamaksym@gmail.com>
2022-07-13 18:04:01 +0200

    GUID resource locator & finished GUID resources

--- Assets/Scripts/IGUIDResources/GUIDResourceLocator.cs
+++ Assets/Scripts/IGUIDResources/GUIDResourceLocator.cs
@@ -1,7 +1,23 @@
-﻿namespace IGUIDResources
+﻿using UnityEngine;
+
+namespace IGUIDResources
 {
     public class GUIDResourceLocator
     {
+        public BikeModels Bikes { get; private set; }
+        public Career Career { get; private set; }
         
+        private GUIDResourceLocator(BikeModels bikes, Career career)
+        {
+            Bikes = bikes;
+            Career = career;
+        }
+
+        public static GUIDResourceLocator Initialize()
+        {
+            BikeModels bikes = Resources.Load<BikeModels>("GUIDResources/Bikes/BikesRegistry");
+            Career career = Resources.Load<Career>("GUIDResources/Career/Career");
+            return new GUIDResourceLocator(bikes, career);
+        }
     }
 }
---
+++ Assets/Scripts/LevelLoading/BikeSelection.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using IGUIDResources;
+using UnityEngine;
+
+namespace LevelLoading
+{
+    public class BikeSelection : MonoBehaviour
+    {
+        private GUIDResourceLocator _resourceLocator;
+        
+        private void Awake()
+        {
+            _resourceLocator = GUIDResourceLocator.Initialize();
+        }
+
+        public static BikeSelection Display()
+        {
+            return null;//TODO display additive scene
+        }
+        
+        public Task<BikeModel> RetrieveSelectedBikeModel()
+        {
+            return Task.FromResult(_resourceLocator.Bikes.GetDefault());
+        }
+    }
+}
