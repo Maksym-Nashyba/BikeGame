a6f93e2c8a870b8ad618264929cd2bc45dc6cfd2
CakeDee <voakovskydmitro@gmail.com>
2022-08-09 15:16:17 +0300

    ShopWindows

--- Assets/Scripts/ProgressionStore/ShopWindows.cs
+++ Assets/Scripts/ProgressionStore/ShopWindows.cs
@@ -5,11 +5,14 @@ namespace ProgressionStore
     public class ShopWindows : MonoBehaviour
     {
         [SerializeField] private ShopWindow[] _windows;
-        private ShopWindow _openedWindow;
 
         public void OpenWindow(int index)
         {
-            
+            for (int  i = 0;  i < _windows.Length;  i++)
+            {
+                if(i != index) _windows[i].Close();
+            }
+            _windows[index].Open();
         }
     }
 }
