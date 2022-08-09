44c5e980d3154d072509aeca6d29a6bcf8bfb023
WingedCryptid <yariksouth@gmail.com>
2022-08-09 13:44:07 +0300

    LocalPersistencyTests

--- Assets/Tests/EditMode/LocalPersistencyTests.cs
+++ Assets/Tests/EditMode/LocalPersistencyTests.cs
@@ -1,13 +1,19 @@
 ﻿using NUnit.Framework;
+using SaveSystem.Models;
+using SaveSystem.PersistencyAndSerialization;
 
 namespace Tests.EditMode
 {
     public class LocalPersistencyTests
     {
         [Test]
-        public void LocalPersistencyTestsSimplePasses()
+        public void SaveAndLoadData()
         {
-            
+            SaveData originalData = MockData.GenerateMockSaveData();
+            LocalFilePersistency<ISaveDataSerializer> filePersistency = new LocalFilePersistency<ISaveDataSerializer>(new BinarySaveDataSerializer());
+            filePersistency.Save(originalData);
+            SaveData loadedSaveData = filePersistency.Load().Result;
+            Assert.True(Equals(originalData, loadedSaveData), "Saved data is not equal to loaded data");
         }
     }
 }
