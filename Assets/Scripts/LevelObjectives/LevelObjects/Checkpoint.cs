bbbe75892ff1a07234ce6210eea74a1d460b5258
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-08-20 13:13:47 +0200

    ...

--- Assets/Scripts/LevelObjectives/LevelObjects/Checkpoint.cs
+++ Assets/Scripts/LevelObjectives/LevelObjects/Checkpoint.cs
@@ -4,10 +4,17 @@ namespace LevelObjectives.LevelObjects
 {
     public class Checkpoint : PlayerTrigger
     {
+        private Vector3 _respawnPoint;
+        
         protected override void OnTriggerEnter(Collider other)
         {
             base.OnTriggerEnter(other);
-            
+            _respawnPoint = other.transform.position;
+        }
+
+        public Vector3 GetRespawnPoint()
+        {
+            return _respawnPoint;
         }
     }
 }
