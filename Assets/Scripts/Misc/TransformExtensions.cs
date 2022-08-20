476594a0303e5cccefccf9085416a56307cdaba6
WingedCryptid <yariksouth@gmail.com>
2022-08-20 15:07:14 +0300

    Created Transformation struct, changed PlayerSpawner, added TransformExtensions

---
+++ Assets/Scripts/Misc/TransformExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    public static class TransformExtensions
+    {
+        public static Transform Apply(this Transform transform, Transformation transformation)
+        {
+            transform.position = transformation.Position;
+            transform.localScale = transformation.Scale;
+            transform.rotation = transformation.Rotation;
+            return transform;
+        }
+    }
+}
---
+++ Assets/Scripts/Misc/Transformation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    public struct Transformation
+    {
+        public Vector3 Position;
+        public Vector3 Scale;
+        public Quaternion Rotation;
+
+        public Transformation(Transform transform)
+        {
+            Position = transform.position;
+            Scale = transform.localScale;
+            Rotation = transform.rotation;
+        }
+    }
+}
