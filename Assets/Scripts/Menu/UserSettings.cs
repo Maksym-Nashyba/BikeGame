e7df673e22a38af85cb82a44fb992c09398c50a0
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-08-23 20:20:43 +0200

    UserSettings facade for player prefs

---
+++ Assets/Scripts/Menu/UserSettings.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Menu
+{
+    public static class UserSettings
+    {
+        private const string _effectsKey = "effects";
+        private const string _ambientKey = "music";
+        private const string _graphicsKey = "graphics";
+
+        public static float GetEffectsVolume()
+        {
+            return PlayerPrefs.GetFloat(_effectsKey, 1f);
+        }
+
+        public static void SetEffectsVolume(float value)
+        {
+            PlayerPrefs.SetFloat(_effectsKey, value);
+            PlayerPrefs.Save();
+        }
+        
+        public static float GetAmbientVolume()
+        {
+            return PlayerPrefs.GetFloat(_ambientKey, 1f);
+        }
+
+        public static void SetAmbientVolume(float value)
+        {
+            PlayerPrefs.SetFloat(_effectsKey, value);
+            PlayerPrefs.Save();
+        }
+
+        public static GraphicsTier GetGraphicsTier()
+        {
+            return (GraphicsTier)PlayerPrefs.GetInt(_graphicsKey, 0);
+        }
+
+        public static void SetGraphicsTier(GraphicsTier graphicsTier)
+        {
+            PlayerPrefs.SetInt(_graphicsKey, (int)graphicsTier);
+        }
+        
+        public enum GraphicsTier
+        {
+            Low,
+            High
+        }
+    }
+}
