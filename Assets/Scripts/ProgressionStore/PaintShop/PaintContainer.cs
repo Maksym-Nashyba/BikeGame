e6a4d69c758ccc0b2ad6d1da8d7972563f81f6ff
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-08-27 15:58:47 +0200

    ....

--- Assets/Scripts/ProgressionStore/PaintShop/PaintContainer.cs
+++ Assets/Scripts/ProgressionStore/PaintShop/PaintContainer.cs
@@ -6,16 +6,19 @@ namespace ProgressionStore.PaintShop
     public class PaintContainer : MonoBehaviour
     {
         [SerializeField] private MeshRenderer _paintRenderer;
+        public Skin CurrentSkin { get; private set; }
 
         public void ShowEmpty()
         {
             _paintRenderer.enabled = false;
+            CurrentSkin = null;
         }
 
         public void ShowWithSkin(Skin skin)
         {
             _paintRenderer.enabled = true;
             _paintRenderer.material = skin.Material;
+            CurrentSkin = skin;
         }
     }
 }
