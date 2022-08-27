ccd3979b89b3330b1a5ef10f4d87d51751f01cf6
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-08-27 23:16:06 +0200

    set up the vending machine rotating button

---
+++ Assets/Scripts/ProgressionStore/PaintShop/PaintShopRotatingButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace ProgressionStore.PaintShop
+{
+    public class PaintShopRotatingButton : MonoBehaviour
+    {
+        public RotatingButtonSides CurrentSide { get; private set; }
+
+        
+        
+        private float SideToRotation(RotatingButtonSides side)
+        {
+            return (int)side * 90f;
+        }
+        
+        public enum RotatingButtonSides
+        {
+            Closed,
+            Buy,
+            Confirm,
+            Select
+        }
+    }
+}
