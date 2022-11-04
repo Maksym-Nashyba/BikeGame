ce04a3c112d2523ee32e5a3f0f016491b6601dd4
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-11-04 13:45:10 +0100

    ...

--- Assets/Scripts/LevelSpecific/IcePeak/WeatherChanger.cs
+++ Assets/Scripts/LevelSpecific/IcePeak/WeatherChanger.cs
@@ -1,4 +1,5 @@
 ﻿using LevelObjectives.LevelObjects;
+using Misc;
 using UnityEngine;
 
 namespace LevelSpecific.IcePeak
@@ -19,18 +20,41 @@ namespace LevelSpecific.IcePeak
         {
             _coldToWarm = _warmSide.position - _coldSide.position;
             _coldToWarmDistance = _coldToWarm.magnitude;
-        }
 
-        private void SetParticlesRate(ParticleSystem particleSystem, float portion)
-        {
-            ParticleSystem.EmissionModule particleSystemEmission = particleSystem.emission;
-            particleSystemEmission.rateOverTime = Mathf.Clamp(_maxEmissionRate*portion -5f, 0, float.MaxValue);
+            ServiceLocator.Player.Respawned += OnPlayerRespawned;
         }
 
         private void OnTriggerStay(Collider other)
         {
             SetParticlesRate(_snowParticles, 1f-(_coldSide.transform.position - other.transform.position).sqrMagnitude/(_coldToWarmDistance * _coldToWarmDistance));
             SetParticlesRate(_rainParticles, 1f-(_warmSide.transform.position - other.transform.position).sqrMagnitude/(_coldToWarmDistance * _coldToWarmDistance));
+        }
+        
+        private void SetParticlesRate(ParticleSystem particleSystem, float portion)
+        {
+            ParticleSystem.EmissionModule particleSystemEmission = particleSystem.emission;
+            particleSystemEmission.rateOverTime = Mathf.Clamp(_maxEmissionRate*portion -5f, 0, float.MaxValue);
+        }
+
+        private void OnPlayerRespawned()
+        {
+            Transform playerTransform = ServiceLocator.Player.ActivePlayerClone.transform;
+            if (Vector2.Distance(playerTransform.position, _coldSide.position) <
+                Vector2.Distance(playerTransform.position, _warmSide.position))
+            {
+                SetParticlesRate(_snowParticles, 1f);
+                SetParticlesRate(_rainParticles, 0f);
+            }
+            else
+            {
+                SetParticlesRate(_rainParticles, 1f);
+                SetParticlesRate(_snowParticles, 0f);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            ServiceLocator.Player.Respawned -= OnPlayerRespawned;
         }
     }
 }
