2139b8f1c262700b3d61c220c3f16df980d0f769
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-11-06 12:29:01 +0100

    smooth camera movement between checkpoints

--- Assets/Scripts/Menu/LevelSelection/LevelSelectionCamera.cs
+++ Assets/Scripts/Menu/LevelSelection/LevelSelectionCamera.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Menu
@@ -6,19 +7,43 @@ namespace Menu
     public class LevelSelectionCamera : MonoBehaviour
     {
         public CameraCheckpoint GetCheckpointFor(int levelIndex) => _checkpoints[levelIndex];
+        [Range(0f, 3f)][SerializeField] private float _transitionDurationSeconds;
         [SerializeField] private Transform _cameraTransform;
         [SerializeField] private CameraCheckpoint[] _checkpoints;
+        private int _currentCheckpoint;
 
         public void TeleportToLevel(int levelIndex)
         {
             Vector3 checkpointPosition = _checkpoints[levelIndex].Position;
             _cameraTransform.SetPositionAndRotation(checkpointPosition, GetLookRotation(checkpointPosition, _checkpoints[levelIndex].Target));
+            _currentCheckpoint = levelIndex;
         }
         
-        public Task MoveToLevel(int targetLevelIndex)
+        public async Task MoveToLevel(int targetLevelIndex)
         {
-            TeleportToLevel(targetLevelIndex);
-            return Task.CompletedTask;
+            while (targetLevelIndex != _currentCheckpoint)
+            {
+                int stepDirection = Convert.ToInt32(_currentCheckpoint < targetLevelIndex) * 2 -1;
+                await MoveByOneCheckpoint(stepDirection);
+                _currentCheckpoint += stepDirection;
+            }
+        }
+
+        private async Task MoveByOneCheckpoint(int direction)
+        {
+            CameraCheckpoint startCheckpoint = _checkpoints[_currentCheckpoint];
+            CameraCheckpoint targetCheckpoint = _checkpoints[_currentCheckpoint+direction];
+            float timePassedFraction = 0f;
+            while (timePassedFraction < 1f)
+            {
+                Vector3 nextPosition = Vector3.Lerp(startCheckpoint.Position, targetCheckpoint.Position, timePassedFraction);
+                Vector3 nextCameraTarget = Vector3.Lerp(startCheckpoint.Target, targetCheckpoint.Target, timePassedFraction);
+                Quaternion nextQuaternion = GetLookRotation(_cameraTransform.position, nextCameraTarget);
+                _cameraTransform.SetPositionAndRotation(nextPosition, nextQuaternion);
+                
+                await Task.Yield();
+                timePassedFraction += Time.deltaTime;
+            }
         }
 
         private Quaternion GetLookRotation(Vector3 from, Vector3 to)
