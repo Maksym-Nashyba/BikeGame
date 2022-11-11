1726a9360da3ffc79188fe47f39bd2244f30cdfb
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-11-12 00:34:07 +0100

    ...

--- Assets/Scripts/ProgressionStore/Computer/TaskIcon.cs
+++ Assets/Scripts/ProgressionStore/Computer/TaskIcon.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ProgressionStore.Computer
 {
     public class TaskIcon : MonoBehaviour
     {
         public event Action<Program> Clicked;
+        [SerializeField] private Image _iconImage;
         private Program _program;
         
         public void SetUp(Program program)
         {
             _program = program;
+            _iconImage.sprite = program.TaskBarProcessSprite;
         }
 
         public void Close()
