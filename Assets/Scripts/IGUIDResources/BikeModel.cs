c18967454610fb1d52f9e03a05d27020459e9dc9
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-11-12 22:11:55 +0100

    ...

--- Assets/Scripts/IGUIDResources/BikeModel.cs
+++ Assets/Scripts/IGUIDResources/BikeModel.cs
@@ -6,8 +6,11 @@ namespace IGUIDResources
     [CreateAssetMenu(fileName = "newBikeModel", menuName = "ScriptableObjects/Bikes/Model")]
     public class BikeModel : ScriptableObject, IGUIDResource
     {
+        public string Name;
         public GameObject Prefab;
+        public GameObject EmptyPrefab;
         public Skin[] AllSkins;
+        public int Cost;
         public float stat1;
         public float stat2;
         public float stat3;
--- Assets/Scripts/ProgressionStore/Computer/Browser/ScamazonTab.cs
+++ Assets/Scripts/ProgressionStore/Computer/Browser/ScamazonTab.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using IGUIDResources;
+using TMPro;
+using UnityEngine;
 
 namespace ProgressionStore.Computer.Browser
 {
     public class ScamazonTab : MonoBehaviour
     {
-        
+        [SerializeField] private BikeModel _bike;
+        [SerializeField] private TextMeshProUGUI _priceText;
+
+        private void Start()
+        {
+            _priceText.SetText($"{_bike.Cost}$");
+        }
     }
 }
