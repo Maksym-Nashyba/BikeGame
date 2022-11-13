3faccfe85966c53d32ce7c3cdde29ef70a03e467
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-11-13 15:03:20 +0100

    Fixed pseudo-raycaster bug

--- Assets/Scripts/ProgressionStore/Computer/Raycastable.cs
+++ Assets/Scripts/ProgressionStore/Computer/Raycastable.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace ProgressionStore.Computer
 {
     public class Raycastable : MonoBehaviour
     {
-        public RectTransform RectTransform { get; private set; }
+        private RectTransform _rectTransform;
+        public RectTransform RectTransform
+        {
+            get
+            {
+                if (_rectTransform == null)
+                {
+                    _rectTransform = GetComponent<RectTransform>();
+                }
+                return _rectTransform;
+            }
+            private set => _rectTransform = value;
+        }
 
         private void Awake()
         {
