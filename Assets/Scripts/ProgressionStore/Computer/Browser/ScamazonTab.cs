6371c6f4cafbaae5283f648ca3e6c260cc7de91d
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-11-13 13:47:44 +0100

    added two bike placeholders

--- Assets/Scripts/ProgressionStore/Computer/Browser/ScamazonTab.cs
+++ Assets/Scripts/ProgressionStore/Computer/Browser/ScamazonTab.cs
@@ -1,18 +1,72 @@
 ﻿using System;
 using IGUIDResources;
+using SaveSystem.Front;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ProgressionStore.Computer.Browser
 {
     public class ScamazonTab : MonoBehaviour
     {
         [SerializeField] private BikeModel _bike;
+        [SerializeField] private Transform _previewPoint;
         [SerializeField] private TextMeshProUGUI _priceText;
+        [SerializeField] private TextMeshProUGUI _nameText;
+        [SerializeField] private Button _buyButton;
+        [SerializeField] private GameObject _soldOutText;
+        private Saves _saves;
+
+        private void Awake()
+        {
+            _saves = FindObjectOfType<Saves>();
+        }
 
         private void Start()
         {
+            SetUpUI(BikeBought());
+        }
+
+        public void OnBuyButton()
+        {
+            if(BikeBought()) return;
+
+            if (TryBuy())
+            {
+                SetUpUI(true);
+            }
+        }
+
+        private void SetUpUI(bool bikeBought)
+        {
             _priceText.SetText($"{_bike.Cost}$");
+            _nameText.SetText($"{_bike.Name}");
+            CreatePreviewModel();
+            _buyButton.interactable = !bikeBought;
+            _soldOutText.SetActive(bikeBought);
+        }
+
+        private bool TryBuy()
+        {
+            if (_saves.Currencies.GetDollans() < _bike.Cost) return false;
+            
+            _saves.Currencies.SubtractDollans(_bike.Cost);
+            _saves.Bikes.UnlockBike(_bike.GetGUID());
+            return true;
+        }
+        
+        private void CreatePreviewModel()
+        {
+            Transform previewModel = Instantiate(_bike.EmptyPrefab, _previewPoint).transform;
+            for (int i = 0; i < previewModel.childCount; i++)
+            {
+                previewModel.GetChild(i).gameObject.layer = LayerMask.NameToLayer("ComputerUI");
+            }
+        }
+        
+        private bool BikeBought()
+        {
+            return _saves.Bikes.IsBikeUnlocked(_bike);
         }
     }
 }
