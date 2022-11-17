9a19290f2441dd04bc6904c2b6f15bb33c0486a7
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-11-17 17:16:55 +0100

    Added back buttons everywhere

---
+++ Assets/Scripts/Garage/Computer/ExitSign.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Misc;
+using UnityEngine.SceneManagement;
+
+namespace Garage.Computer
+{
+    public class ExitSign : ClickTarget<ExitSign>
+    {
+        public override event Action<ExitSign> Clicked;
+        protected override void OnClicked()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
