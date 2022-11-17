797b316a61a2f04f1596f1dc9f13f738f2a873fd
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-11-17 19:29:40 +0100

    ...

--- Assets/Scripts/Garage/BikePreview.cs
+++ Assets/Scripts/Garage/BikePreview.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using IGUIDResources;
 using Misc;
 using SaveSystem.Front;
+using SaveSystem.Models;
 using UnityEngine;
 
 namespace Garage
@@ -13,12 +15,20 @@ namespace Garage
         [SerializeField] private BikeModelDisplay _display;
         private Saves _saves;
         private BikeModel _currentModel;
+        private GUIDResourceLocator _resourceLocator;
 
         private void Awake()
         {
+            _resourceLocator = GUIDResourceLocator.InitializeBikes();
             _saves = FindObjectOfType<Saves>();
             _checkpoint.CameraArrived += OnCameraArrived;
             _checkpoint.CameraDeparted += OnCameraDeparted;
+        }
+
+        private void Start()
+        {
+            _currentModel = PickFirstModel();
+            _display.Display(_currentModel, _saves.Bikes.GetSelectedSkinFor(_currentModel));
         }
 
         private void OnDestroy()
@@ -36,7 +46,7 @@ namespace Garage
         {
             _garageUI.RightLeftButtonClicked -= OnNavigationButton;
         }
-        
+
         private void OnNavigationButton(Direction1D direction)
         {
             _currentModel = PickNextModel(direction);
@@ -45,7 +55,28 @@ namespace Garage
 
         private BikeModel PickNextModel(Direction1D direction)
         {
-            throw new NotImplementedException();
+            PersistentBike[] allUnlockedBiked = _saves.Bikes.GetAllUnlockedBikes();
+            int currentIndex = Array.FindIndex(allUnlockedBiked, bike => bike.GUID == _currentModel.GetGUID());
+            int nextIndex = GetNextIndexWithWraparound(allUnlockedBiked.Length, currentIndex, direction);
+            return _resourceLocator.Bikes.Get(allUnlockedBiked[nextIndex].GUID);
+        }
+
+        private int GetNextIndexWithWraparound(int length, int current, Direction1D direction)
+        {
+            if (length == 1) return current;
+            if (direction == Direction1D.Left)
+            {
+                if (current == 0) return --length;
+                return --current;
+            }
+
+            if (current == length - 1) return 0;
+            return ++current;
+        }
+
+        private BikeModel PickFirstModel()
+        {
+            return _resourceLocator.Bikes.Get(_saves.Bikes.GetAllUnlockedBikes()[0].GUID);
         }
     }
 }
