7b2269abf91a933429f69cf183c27bef84a24f0d
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-12-07 21:50:21 +0100

    Garage achievement props

---
+++ Assets/Scripts/Menu/Garage/AchievementProp.cs
@@ -0,0 +1,17 @@
+﻿using IGUIDResources;
+using SaveSystem.Front;
+using UnityEngine;
+
+namespace Menu.Garage
+{
+    public class AchievementProp : MonoBehaviour
+    {
+        [SerializeField] private Level _level;
+
+        private void Start()
+        {
+            Saves saves = FindObjectOfType<Saves>();
+            if(!saves.Career.IsCompleted(_level)) Destroy(gameObject); 
+        }
+    }
+}
