2d2932ea95f39f49d75e8df2f745fd54f6db657a
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-12-09 11:47:44 +0100

    Production checkpoints

--- Assets/Scripts/LevelObjectives/LevelObjects/Checkpoint.cs
+++ Assets/Scripts/LevelObjectives/LevelObjects/Checkpoint.cs
@@ -1,7 +1,22 @@
-﻿namespace LevelObjectives.LevelObjects
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace LevelObjectives.LevelObjects
 {
     public class Checkpoint : PlayerTrigger
     {
-        
+        [SerializeField] private UnityEvent _checkpointActivated;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Activated += OnCheckpointActivated;
+        }
+
+        private void OnCheckpointActivated()
+        {
+            Activated -= OnCheckpointActivated;
+            _checkpointActivated.Invoke();
+        }
     }
 }
