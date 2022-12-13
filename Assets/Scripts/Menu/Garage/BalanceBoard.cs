a838e5af49db28ff6d781bffa4f444820e55cc7d
WingedCryptid <yariksouth@gmail.com>
2022-12-13 21:44:21 +0200

    Added balance board

---
+++ Assets/Scripts/Menu/Garage/BalanceBoard.cs
@@ -0,0 +1,33 @@
+﻿using SaveSystem.Front;
+using TMPro;
+using UnityEngine;
+
+namespace Menu.Garage
+{
+    public class BalanceBoard : MonoBehaviour
+    {
+        [SerializeField] private TextMeshPro _balanceOnBoard;
+        private Saves _saves;
+
+        private void Awake()
+        {
+            _saves = FindObjectOfType<Saves>();
+            _saves.Currencies.Changed += OnCurrenciesChanged;
+        }
+
+        private void Start()
+        {
+            OnCurrenciesChanged();
+        }
+
+        private void OnCurrenciesChanged()
+        {
+            _balanceOnBoard.text = _saves.Currencies.GetDollans() + "$";
+        }
+
+        private void OnDestroy()
+        {
+            _saves.Currencies.Changed -= OnCurrenciesChanged;
+        }
+    }
+}
