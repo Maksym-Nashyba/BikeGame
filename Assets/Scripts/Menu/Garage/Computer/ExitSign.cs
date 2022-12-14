3ce374aa2f720b3a241890c7ba87203076c3024e
CakeDee <103380263+CakeDee@users.noreply.github.com>
2022-12-14 14:27:16 +0200

    garage

--- Assets/Scripts/Menu/Garage/Computer/ExitSign.cs
+++ Assets/Scripts/Menu/Garage/Computer/ExitSign.cs
@@ -2,16 +2,20 @@
 using Effects.TransitionCover;
 using Misc;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 namespace Menu.Garage.Computer
 {
     public class ExitSign : ClickTarget<ExitSign>
     {
+        public UnityEvent EditorClicked;
         public override event Action<ExitSign> Clicked;
         [SerializeField] private SceneTransitionCover _transitionCover;
+        
         protected override async void OnClicked()
         {
+            EditorClicked.Invoke();
             await _transitionCover.TransitionToState(SceneTransitionCover.State.Covered);
             SceneManager.LoadScene("MainMenu");
         }
