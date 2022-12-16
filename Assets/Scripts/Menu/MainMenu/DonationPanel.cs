fc744b0269c8a9a5f634b5ce1ebfb7bd6b50c4c0
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-12-17 00:40:22 +0100

    donation panel visual

---
+++ Assets/Scripts/Menu/MainMenu/DonationPanel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using SaveSystem.Front;
+using UnityEngine;
+
+namespace Menu
+{
+    public class DonationPanel : MonoBehaviour
+    {
+        private const string _playerPrefsKey = "DonationPanelShown";
+        private Saves _saves;
+
+        private void Awake()
+        {
+            _saves = FindObjectOfType<Saves>();
+        }
+
+        private void Start()
+        {
+            if (GameComplete() && AlreadyShown())
+            {
+                Show();
+            }
+        }
+
+        private void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool AlreadyShown()
+        {
+            return PlayerPrefs.HasKey(_playerPrefsKey);
+        }
+
+        private bool GameComplete()
+        {
+            return _saves.Career.GetAllCompletedLevels().Length >= 4;
+        }
+    }
+}
